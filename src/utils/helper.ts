5ad0a4080f118db766150cbd143ab2a7f3d4580c
spacemeowx2 <spacemeowx2@gmail.com>
2018-10-04 23:05:28 +0800

    new shuffle

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,5 +1,11 @@
 export function shuffle<T> (ary: T[]) {
-  return ary.sort((a, b) => Math.random() - 0.5)
+  let r = [...ary]
+  let i = r.length
+  while (i) {
+    const j = Math.floor(Math.random() * i--);
+    [r[i], r[j]] = [r[j], r[i]]
+  }
+  return r
 }
 export function exclude<T> (ary: T[], exc: T[]) {
   return ary.filter(i => !exc.includes(i))
