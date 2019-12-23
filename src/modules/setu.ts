df1b06cbedb76a6bd3ec6585105b5de015e23286
spacemeowx2 <spacemeowx2@gmail.com>
2019-12-24 00:18:03 +0800

    add setu with delay

--- src/modules/setu.ts
+++ src/modules/setu.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 import { BaseBotModule, BotMessageEvent, BotModuleInitContext } from '../interface'
-import { cqStringify, cqCode } from '../utils/cqcode'
+import { cqCode, cql } from '../utils/cqcode'
 import { getImage } from '../utils/getImage'
 
 interface Response {
@@ -14,20 +14,34 @@ export class SeTu extends BaseBotModule {
   id = 'SeTu'
   name = ''
   defaultEnable = true
+  map: Map<number, number | undefined> = new Map()
 
   init (ctx: BotModuleInitContext) {
     super.init(ctx)
     const { bus } = ctx
-    bus.registerMessage([bus.startsWithFilter('.色图')], e => this.onMsg(e))
+    bus.registerMessage([bus.groupTypeFilter, bus.startsWithFilter('.色图')], e => this.onMsg(e))
+  }
+  checkDelay(groupId: number) {
+    const now = Math.floor(Date.now() / 1000)
+
+    const time = this.map.get(groupId)
+    if (now - (time || 0) >= 60) {
+      this.map.set(groupId, now)
+      return true
+    }
+    return false
   }
   async onMsg (e: BotMessageEvent) {
     console.log('SeTu', e.userId)
+    if (!this.checkDelay(e.groupId!)) {
+      return cql`${cqCode('at', { qq: e.userId.toString() })} 不要太频繁`
+    }
     const h = new Date().getHours()
     if (h >= 0 && h <= 6) {
       try {
         const setu = await axios.get<Response>('https://api.lolicon.app/setu/?size1200=true')
 
-        return cqStringify([await getImage(setu.data.data[0].url)])
+        return cql`${cqCode('at', { qq: e.userId.toString() })} ${await getImage(setu.data.data[0].url)}`
       } catch (e) {
         return 'Error: ' + e.toString()
       }
