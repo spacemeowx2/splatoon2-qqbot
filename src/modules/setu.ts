a86357f379c15434800911ad8ae0971cef19a473
spacemeowx2 <spacemeowx2@gmail.com>
2020-01-05 19:57:23 +0800

    setu down

--- src/modules/setu.ts
+++ src/modules/setu.ts
@@ -1,7 +1,4 @@
-import axios from 'axios'
 import { BaseBotModule, BotMessageEvent, BotModuleInitContext } from '../interface'
-import { cqCode, cql } from '../utils/cqcode'
-import { getImage } from '../utils/getImage'
 
 interface Response {
   code: number
@@ -21,33 +18,8 @@ export class SeTu extends BaseBotModule {
     const { bus } = ctx
     bus.registerMessage([bus.groupTypeFilter, bus.startsWithFilter('.色图')], e => this.onMsg(e))
   }
-  checkDelay(groupId: number) {
-    const now = Math.floor(Date.now() / 1000)
-
-    const time = this.map.get(groupId)
-    if (now - (time || 0) >= 60) {
-      this.map.set(groupId, now)
-      return true
-    }
-    return false
-  }
   async onMsg (e: BotMessageEvent) {
-    console.log('SeTu', e.userId)
-    if (!this.checkDelay(e.groupId!)) {
-      return cql`${cqCode('at', { qq: e.userId.toString() })} 不要太频繁`
-    }
-    const h = new Date().getHours()
-    if (h >= 0 && h <= 6) {
-      try {
-        const setu = await axios.get<Response>('https://api.lolicon.app/setu/?size1200=true')
-
-        return cql`${cqCode('at', { qq: e.userId.toString() })} ${await getImage(setu.data.data[0].url)}`
-      } catch (e) {
-        return 'Error: ' + e.toString()
-      }
-    } else {
-      return '现在还不是色图时间'
-    }
+    return '亏我这么信任你们. 这个功能永久下线了.'
   }
   help () {
     return ''
